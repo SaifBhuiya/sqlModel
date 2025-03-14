f7317b6937f02d7296979aee83b59e6fad25d497
SaifBhuiya <bhuiyasaif@gmail.com>
2025-03-15 05:43:28 +0600

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,5 @@
 import { useEffect, useState } from "react";
-import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend,
-    BarElement
-} from "chart.js";
-
-// Register them
-ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend,
-    BarElement
-);
+import { Chart as Chartjs } from "chart.js/auto";
 import { Line } from "react-chartjs-2";
 
 
